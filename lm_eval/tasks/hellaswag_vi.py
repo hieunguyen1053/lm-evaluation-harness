effbc033a78e76257570313d5f2483ec857fb7c0
Hieu Nguyen <hieunguyen1053@outlook.com>
2023-10-08 17:29:04 +0900

    update hellaswag

--- lm_eval/tasks/hellaswag_vi.py
+++ lm_eval/tasks/hellaswag_vi.py
@@ -4,3 +4,12 @@ class HellaSwag_vi(HellaSwag):
     VERSION = 0
     DATASET_PATH = "vlsp-2023-vllm/hellaswag"
     DATASET_NAME = None
+
+    def has_training_docs(self):
+        return False
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return False
